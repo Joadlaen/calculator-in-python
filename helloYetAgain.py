327f656ef52ea26a8a7900d7059879390c04d597
Joadlaen <jackcullenmurphy1999@gmail.com>
2024-09-25 13:02:08 +0100

    improvements

--- helloYetAgain.py
+++ helloYetAgain.py
@@ -1,5 +1,37 @@
 print("Hello again World")
 
-primary = int(input("First Number Please"))
-secondary = int(input("Second Number Please"))
-function = input("please input function")
+#functions are used to decrease the RAM usage
+
+def addition(first, second):
+    sum = first + second
+    print(sum)
+
+def subtraction(first, second):
+    diff = first - second
+    print(diff)
+
+def multiplication(first, second):
+    product = first*second
+    print(product)
+
+def division(first, second):
+    quotient = first/second
+    print(quotient)
+#infinite loop
+while (1>0):
+    primary = int(input("First Number Please: \n"))
+    secondary = int(input("Second Number Please: \n"))
+    function = input("please input function: \n")
+    print(primary%secondary)
+#
+    if (function == "add"):
+        addition(primary, secondary)
+
+    elif (function == "subtract"):
+        subtraction(primary, secondary)
+
+    elif (function == "multiply"):
+        multiplication(primary, secondary)
+
+    elif (function == "divide"):
+        division(primary, secondary)
---
+++ text.py
@@ -0,0 +1,68 @@
+#self explanatory
+#task 1.1
+print("Hello World! is cool!")
+
+#task 1.2 suitable comments to improve maintainability
+
+# This is a single line comment in Python
+
+print("single line comments start with")
+
+# print("Hello")
+# print("World")
+# print("with Comments")
+
+
+#1.3 
+
+print(type(10))
+print(type(3.141592))
+print(type("hello world"))
+print(type(True))
+
+object1 = 10
+object2 = "Bull"
+print(type(object2))
+
+print(type(12+5))
+print(type(5+3.0))
+print(type(12/5))
+print(type(12+5==5*3.0))
+
+
+#1.4
+#errors and exceptions
+
+#print(Hello World)
+#syntax error
+
+#printy("Hello World")
+#name error
+
+# You need to fix the following lines
+# Run the code and then use the error messages to fix each line
+
+# This line has a SyntaxError
+print("hello world")
+
+# This line has a NameError
+print("Hello World!")
+
+# The following lines cause a TypeError
+int1 = 100
+str1 = 10
+print(int1 / str1)
+
+#1.5
+
+
+# This line is not indented properly. Fix this
+print("Indentation matters in Python")
+
+# You will notice that we have blank lines
+# Python doesn't care about these!
+
+# There is extra whitespace inside the print statement 
+# Python doesn't care about this
+print("Hello World!"  )
+
